5be86647c997f9774db16aacc80a2856c7e8d906
joshuageronimo <joshuag31@hotmail.com>
2018-02-14 17:23:42 -0500

    Rename ViewController to NewsViewController, Add two new VCs [Crypto & Developer], Fix spacing of the tab bar images

---
+++ Crypto Headlines/Controller/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  Crypto Headlines
+//
+//  Created by Joshua Geronimo on 2/14/18.
+//  Copyright © 2018 Joshua Geronimo. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
